7bc9f06e5b57c440a5218632a164b4f27c8a75a9
gotthejuice <hmitss2000@gmail.com>
2026-06-18 19:07:16 +0300

    Disable drop cap on abituriyentu section pages

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -25,7 +25,7 @@
         @endif
 
         @if (filled($page->body))
-            <x-prose.article :heritage="$page->is_heritage">
+            <x-prose.article :heritage="$page->is_heritage" :drop-cap="$page->section !== 'abituriyentu'">
                 {!! $page->body !!}
             </x-prose.article>
         @endif
